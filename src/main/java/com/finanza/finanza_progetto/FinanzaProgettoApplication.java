6c6060134dce288e472538ac943d6317c6faf899
federicodececco <federicodececco1@gmail.com>
2025-05-21 11:07:20 +0200

    first commit

---
+++ src/main/java/com/finanza/finanza_progetto/FinanzaProgettoApplication.java
@@ -0,0 +1,13 @@
+package com.finanza.finanza_progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanzaProgettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanzaProgettoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finanza/finanza_progetto/FinanzaProgettoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finanza.finanza_progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzaProgettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
