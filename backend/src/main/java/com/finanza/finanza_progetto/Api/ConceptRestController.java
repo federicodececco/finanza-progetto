a704e2873ca5e702a8b4df1761f247c003f9c9c7
federicodececco <federicodececco1@gmail.com>
2025-05-23 09:39:27 +0200

    moved directories

---
+++ backend/src/main/java/com/finanza/finanza_progetto/Api/ConceptRestController.java
@@ -0,0 +1,12 @@
+package com.finanza.finanza_progetto.Api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/api/concepts")
+public class ConceptRestController {
+
+}
---
+++ backend/src/main/java/com/finanza/finanza_progetto/service/ConceptService.java
@@ -0,0 +1,37 @@
+package com.finanza.finanza_progetto.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.finanza.finanza_progetto.model.Concept;
+import com.finanza.finanza_progetto.repository.ConceptRepository;
+
+@Service
+public class ConceptService {
+
+    @Autowired
+    ConceptRepository conceptRepository;
+
+    public List<Concept> findAll() {
+        return conceptRepository.findAll();
+    }
+
+    public Optional<Concept> findById(Integer id) {
+        return conceptRepository.findById(id);
+    }
+
+    public Concept create(Concept concept) {
+        return conceptRepository.save(concept);
+    }
+
+    public Concept edit(Concept concept) {
+        return conceptRepository.save(concept);
+    }
+
+    public void deleteById(Integer id) {
+        conceptRepository.deleteById(id);
+    }
+}
